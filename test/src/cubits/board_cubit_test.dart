c5724a028a73b9347969f9c1fff7f0e803192cc5
isaacscardoso <isaacscardosoblues@gmail.com>
2024-03-07 00:37:57 -0300

    feat(tests): Implements data addition and deletion tests.

--- test/src/cubits/board_cubit_test.dart
+++ test/src/cubits/board_cubit_test.dart
@@ -1,6 +1,7 @@
-import 'package:mocktail/mocktail.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
+import 'package:todo_list_app/src/models/task.dart';
 import 'package:todo_list_app/src/cubits/board_cubit.dart';
 import 'package:todo_list_app/src/states/board_state.dart';
 import 'package:todo_list_app/src/repositories/board_repository.dart';
@@ -10,10 +11,12 @@ final class BoardRepositoryMocked extends Mock implements BoardRepository {}
 void main() {
   late BoardCubit systemUnderTest;
   late BoardRepository repository;
+  late Task task;
 
   setUp(() {
     repository = BoardRepositoryMocked();
     systemUnderTest = BoardCubit(repository: repository);
+    task = const Task(id: 1, description: '', isChecked: false);
   });
 
   tearDown(() => reset(repository));
@@ -24,10 +27,7 @@ void main() {
 
       expect(
         systemUnderTest.stream,
-        emitsInOrder([
-          isA<LoadingBoardState>(),
-          isA<GettedTasksBoardState>(),
-        ]),
+        emitsInOrder([isA<LoadingBoardState>(), isA<GettedTasksBoardState>()]),
       );
 
       await systemUnderTest.fetchTasks();
@@ -38,13 +38,69 @@ void main() {
 
       expect(
         systemUnderTest.stream,
-        emitsInOrder([
-          isA<LoadingBoardState>(),
-          isA<FailureBoardState>(),
-        ]),
+        emitsInOrder([isA<LoadingBoardState>(), isA<FailureBoardState>()]),
       );
 
       await systemUnderTest.fetchTasks();
+    });
+  });
+
+  group('addTask', () {
+    test('Should add a task.', () async {
+      when(() => repository.update(any())).thenAnswer((_) async => []);
+
+      expect(
+        systemUnderTest.stream,
+        emitsInOrder([isA<GettedTasksBoardState>()]),
+      );
+
+      await systemUnderTest.addTask(task);
+      final state = systemUnderTest.state as GettedTasksBoardState;
+
+      expect(state.tasks.length, equals(1));
+      expect(state.tasks, equals([task]));
+    });
+
+    test('Should throw an exception when an error occurs.', () async {
+      when(() => repository.update(any())).thenThrow(Exception('Error'));
+
+      expect(
+        systemUnderTest.stream,
+        emitsInOrder([isA<FailureBoardState>()]),
+      );
+
+      await systemUnderTest.addTask(task);
+    });
+  });
+
+  group('deleteTask', () {
+    test('Should delete a task.', () async {
+      when(() => repository.update(any())).thenAnswer((_) async => []);
+
+      expect(
+        systemUnderTest.stream,
+        emitsInOrder([isA<GettedTasksBoardState>()]),
+      );
+
+      systemUnderTest.addTasksForTest([task]);
+      var state = systemUnderTest.state as GettedTasksBoardState;
+
+      expect(state.tasks.length, equals(1));
+      await systemUnderTest.deleteTask(task);
+
+      state = systemUnderTest.state as GettedTasksBoardState;
+      expect(state.tasks.length, equals(0));
+    });
+
+    test('Should throw an exception when an error occurs.', () async {
+      when(() => repository.update(any())).thenThrow(Exception('Error'));
+
+      expect(
+        systemUnderTest.stream,
+        emitsInOrder([isA<FailureBoardState>()]),
+      );
+
+      await systemUnderTest.deleteTask(task);
     });
   });
 }
