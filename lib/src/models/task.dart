95a6a3ab33ea681169d0ec3529d5191e5f20effe
isaacscardoso <isaacscardosoblues@gmail.com>
2024-03-23 02:09:56 -0300

    feat(Task): Altera o nome do atributo 'isChecked' para 'isCompleted'.

--- lib/src/models/task.dart
+++ lib/src/models/task.dart
@@ -3,26 +3,26 @@ import 'package:equatable/equatable.dart';
 final class Task extends Equatable {
   final int id;
   final String description;
-  final bool isChecked;
+  final bool isCompleted;
 
   const Task({
     required this.id,
     required this.description,
-    required this.isChecked,
+    required this.isCompleted,
   });
 
   @override
-  List<Object?> get props => [id, description, isChecked];
+  List<Object?> get props => [id, description, isCompleted];
 
   Task copyWith({
     int? id,
     String? description,
-    bool? isChecked,
+    bool? isCompleted,
   }) {
     return Task(
       id: id ?? this.id,
       description: description ?? this.description,
-      isChecked: isChecked ?? this.isChecked,
+      isCompleted: isCompleted ?? this.isCompleted,
     );
   }
 }
