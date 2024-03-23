9ed843dd6ed119749706cd633150b0f5fe0c5734
isaacscardoso <isaacscardosoblues@gmail.com>
2024-03-23 18:48:13 -0300

    feat(test): Implements the update test in IsarRepositoryTest.

--- test/src/repositories/isar/isar_board_repository_test.dart
+++ test/src/repositories/isar/isar_board_repository_test.dart
@@ -1,5 +1,6 @@
 import 'package:mocktail/mocktail.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:todo_list_app/src/models/task.dart';
 
 import 'package:todo_list_app/src/repositories/board_repository.dart';
 import 'package:todo_list_app/src/repositories/isar/isar_datasource.dart';
@@ -26,5 +27,16 @@ void main() {
 
     final tasks = await systemUnderTest.fetch();
     expect(tasks.length, equals(1));
+  });
+
+  test('update', () async {
+    when(() => datasource.deleteAllTasks()).thenAnswer((_) async => []);
+    when(() => datasource.putAllTasks(any())).thenAnswer((_) async => []);
+
+    final tasks = await systemUnderTest.update(const [
+      Task(id: -1, description: '', isCompleted: false),
+      Task(id: 2, description: '', isCompleted: false),
+    ]);
+    expect(tasks.length, equals(2));
   });
 }
